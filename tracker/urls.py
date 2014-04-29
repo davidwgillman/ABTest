12eb15acbee27edecae243e787fe0bd02cf9079a
davidwgillman <dgillman@ncf.edu>
2014-04-29 17:19:57 -0400

    first view: patient list

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from tracker import views
 
 urlpatterns = patterns('',
-    # ex: /tracker/5/
+    # ex: /tracker/
     url(r'^$', views.index, name='index'),
     # ex: /tracker/5/
     url(r'^(?P<patient_id>\d+)/$', views.detail, name='detail')
--- tracker/views.py
+++ tracker/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
+from tracker.models import Step, StepOutcome, NextStepCondition, FlagCondition
+from tracker.models import Patient, PatientStep, PatientOutcome
+
 def index(request):
-    return HttpResponse("Hello and welcome to the Tracker.")
+	patientList = Patient.objects.order_by('name')
+	output = ', '.join([unicode((p.name, p.dob)) for p in patientList])
+    	return HttpResponse(output)
 
 def detail(request, patient_id):
     return HttpResponse("You're looking at patient %s." % patient_id)
+
 
